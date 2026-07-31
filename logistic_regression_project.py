256574acbe0f4e6a267774a2d5b216def0a32795
manjulead4 <manjulead4@users.noreply.github.com>
2026-07-31 21:37:21 +0530

    updated code

--- logistic_regression_project.py
+++ logistic_regression_project.py
@@ -29,7 +29,7 @@ y = df["pass"] # target
 
 # train - test split on input features 
 
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42,stratify=y)
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
 
 
 #step 4 : train the logistic regression model 
